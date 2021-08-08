48c55ca5b0d246ae403623af1b7bef184a104cc0
Richard Vlas <vlasrichard@gmail.com>
2021-08-08 05:20:36 +0000

    updated logs script

--- logs.py
+++ logs.py
@@ -1,8 +1,8 @@
-from azureml.core import workspace
+from azureml.core import Workspace
 from azureml.core.webservice import Webservice
 
 # Requires the config to be downloaded first to the current working directory
-ws = workspace.from_config()
+ws = Workspace.from_config()
 
 # Set with the deployment name
 name = "automl-votingensemble"
@@ -12,7 +12,7 @@ service = Webservice(name=name, workspace=ws)
 
 service.update(enable_app_insights=True)
 
-log = service.get_logs()
+logs = service.get_logs()
 
 for line in logs.split('\n'):
     print(line)
